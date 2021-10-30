8b7df2536837cc68a7fdfd457c52b144659cac37
Samyak Shah <samyakshah123@gmail.com>
2021-10-30 13:27:18 +0530

    Authentication implemented

--- web_app/src/services/actions/emailPasswordAuthProvider.js
+++ web_app/src/services/actions/emailPasswordAuthProvider.js
@@ -4,56 +4,50 @@ import { LOGIN_FAIL, LOGIN_SUCCESS } from "./type";
 
 const auth = getAuth();
 
-const registerUserWithEmailPassword = (email, password, displayName, enqueueSnackbar) => dispatch => {
+const registerUserWithEmailPassword =
+  (email, password, displayName, enqueueSnackbar) => (dispatch) => {
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
+      .then((userCredential) => {
+        // Signed in
         const user = userCredential.user;
         const data = user.providerData[0];
         data.displayName = displayName;
-        return registerUser(data).then(res => {
-            if (res.status === 200) {
-                // login success
-                enqueueSnackbar(`${res.data.msg} | ${displayName}`, {
-                  variant: "success",
-                  autoHideDuration: 6000,
-                });
-                dispatch({
-                  type: LOGIN_SUCCESS,
-                  payload: {
-                    user: res.data.user,
-                  },
-                });
-              } else {
-                // login failed
-                console.log(res);
-                enqueueSnackbar(`${res.data.message}`, {
-                  variant: "error",
-                  autoHideDuration: 6000,
-                });
-                dispatch({
-                  type: LOGIN_FAIL,
-                  payload: {
-                    user: null,
-                  },
-                });
+        return registerUser(data).then((res) => {
+          if (res.status === 200) {
+            // login success
+            enqueueSnackbar(`${res.data.msg} | ${displayName}`, {
+              variant: "success",
+              autoHideDuration: 6000,
+            });
+            enqueueSnackbar(
+              `${displayName} updates will be notified on the mail`,
+              {
+                variant: "info",
+                autoHideDuration: 10000,
               }
-                  enqueueSnackbar(`${displayName} updates will be notified on the mail`, {
-                      variant: "info",
-                      autoHideDuration: 10000,
-                  });
-              });
-        })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // log error
-        console.log(error);
-        enqueueSnackbar(error.message, {
-            variant: "error",
-            autoHideDuration: 4000,
+            );
+            dispatch({
+              type: LOGIN_SUCCESS,
+              payload: {
+                user: res.data.user,
+              },
+            });
+          }
         });
-    });
-};
+      })
+      .catch((error) => {
+        // log error
+        dispatch({
+          type: LOGIN_FAIL,
+          payload: {
+            user: null,
+          },
+        });
+        enqueueSnackbar(error?.response?.data?.msg || "User already exists", {
+          variant: "error",
+          autoHideDuration: 4000,
+        });
+      });
+  };
 
 export default registerUserWithEmailPassword;
--- web_app/src/services/actions/googleAuthProvider.js
+++ web_app/src/services/actions/googleAuthProvider.js
@@ -1,8 +1,8 @@
 import {
-	getAuth,
-	signInWithRedirect,
-	signInWithPopup,
-	GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "firebase/auth";
 import Email from "./emailPasswordAuthProvider";
 import { LOGIN_FAIL, LOGIN_SUCCESS } from "./type";
@@ -11,63 +11,57 @@ import { registerUser } from "./auth";
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 const googleSignInWithProps = (enqueueSnackbar) => (dispatch) => {
-	signInWithPopup(auth, provider)
-		.then(async (result) => {
-			// This gives you a Google Access Token. You can use it to access the Google API.
-			const credential = GoogleAuthProvider.credentialFromResult(result);
-			const token = credential.accessToken;
-			// The signed-in user info.
-			const user = result.user;
-			const res = await registerUser(user.providerData[0]);
-			if (res.status === 200) {
-				// login success
-				enqueueSnackbar(`${res.data.msg} | ${user.displayName}`, {
-					variant: "success",
-					autoHideDuration: 6000,
-				});
-				dispatch({
-					type: LOGIN_SUCCESS,
-					payload: {
-						user: res.data.user,
-					},
-				});
-			} else {
-				// login failed
-				enqueueSnackbar(`${res.data.message}`, {
-					variant: "error",
-					autoHideDuration: 6000,
-				});
-				dispatch({
-					type: LOGIN_FAIL,
-					payload: {
-						user: null,
-					},
-				});
-			}
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      return registerUser(user.providerData[0]).then((res) => {
+        if (res.status === 200) {
+          // login success
+          enqueueSnackbar(`${res.data.msg} | ${user.displayName}`, {
+            variant: "success",
+            autoHideDuration: 6000,
+          });
+          enqueueSnackbar(
+            `${user.displayName} updates will be notified on the mail`,
+            {
+              variant: "info",
+              autoHideDuration: 6000,
+            }
+          );
+          dispatch({
+            type: LOGIN_SUCCESS,
+            payload: {
+              user: res.data.user,
+            },
+          });
+        }
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: {
+          user: null,
+        },
+      });
+      enqueueSnackbar(error.response.data.msg, {
+        variant: "error",
+        autoHideDuration: 5000,
+      });
 
-			enqueueSnackbar(
-				`${user.displayName} updates will be notified on the mail`,
-				{
-					variant: "info",
-					autoHideDuration: 10000,
-				}
-			);
-		})
-		.catch((error) => {
-			enqueueSnackbar(error.message, {
-				variant: "error",
-			});
-
-			// Handle Errors here.
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			// The email of the user's account used.
-			const email = error.email;
-			// The AuthCredential type that was used.
-			const credential = GoogleAuthProvider.credentialFromError(error);
-			// Log the error to the console.
-			console.log(errorCode, errorMessage, email, credential);
-		});
+      // Handle Errors here.
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // The email of the user's account used.
+      // const email = error.email;
+      // The AuthCredential type that was used.
+      // const credential = GoogleAuthProvider.credentialFromError(error);
+      // Log the error to the console.
+    });
 };
 
 export default googleSignInWithProps;
